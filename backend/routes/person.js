3afa1b46a8b448aeafdce940e76a20e889f61949
sean1515 <shondaniel89@gmail.com>
2019-01-08 18:23:20 +0200

    graphql-mockup  (#7)
    * #2 add /api end point
    
    * Feat: Create end point to person and discussion.
    
    Created routes files and middleware.
    
    resolves #2
    
    * feat: create person page and home page with routing.
    
    created necessary folder and files.
    added routing using react-router-dom.
    nav-bar with hamburger + side-bar
    
    resolved #1
    
    * feat: mockup api to retrive soldeirs data.
    
    created mockup api using graphql to retrive soldiers data.
    
    resoled #5.
    
    * refactor: change names of variables.
    
    change all bad names to good names.
    
    resolved #7
    
    * fix: delete graphql mockup from the repo.
    
    decided that graphql mockup does not belong to this project.

--- backend/routes/person.js
+++ backend/routes/person.js
@@ -7,4 +7,12 @@ router.get('/', (req, res, next) => {
     })
 });
 
+router.get('/_find', (req, res, next) => {
+    let idArray = Array.isArray(req.query.id) ? req.query.id : [req.query.id];
+    console.log(idArray);
+    res.status(200).json({
+        type: 'aviv gay'
+    })
+});
+
 module.exports = router;
