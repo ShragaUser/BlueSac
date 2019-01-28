27d6322714495017b80672c1eca395a6439e4f0e
ShragaUser <dandankidon@gmail.com>
2019-01-28 16:50:57 +0200

    Update utils.js

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -3,9 +3,7 @@ let utils = {};
 utils.checkRequest = checkRequest;
 
 function checkRequest(req, params) {
-    return params.every(param => {
-        return req.body[param]
-    });
+    return params.every(param => req.body[param]);
 }
 
 module.exports = utils;
