1e50b78040bb4715404c7d906dc9f03ead159efd
sean1515 <shondaniel89@gmail.com>
2019-03-03 16:01:03 +0200

    so i can work from another pc.

---
+++ frontend/src/Components/AddDiscussion/StepperBar/DiscussionStep/DiscussionStep.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+class DiscussionStep extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            discussion: {},
+            fields: this.initFields()
+        }
+    }
+
+    initFields = () => ([
+        {id: 'name', placeHolder: 'שם דש"ב'},
+        {id: 'date', placeHolder: 'תאריך'}
+    ]);
+
+    handleChange = (event) => {
+        let discussion = this.state.discussion;
+        discussion[event.target.id] = event.target.value;
+        this.setState({discussion: discussion})
+    };
+
+    handleClick = () => {
+
+        this.props.handleNext();
+    };
+
+    render() {
+        return (
+            <div>
+                {this.state.fields.map(field => (
+                    <TextField
+                        id={field.id}
+                        placeholder={field.placeHolder}
+                        onChange={this.handleChange}
+                        fullWidth
+                    />
+                ))}
+                <div>
+                    <Button onClick={this.handleClick}>
+                        המשך
+                    </Button>
+                    <Button>
+                        ביטול
+                    </Button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default DiscussionStep;
